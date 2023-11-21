7d47e65ec90d2c5ff437625508e1eab9f66b06fb
khireddinemahaline <khirozr@gmail.com>
2023-11-21 00:44:21 -0800

    s

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this is the entry point of the code
+ * @argc : number of argument
+ * @argv : array of argument
+ * Return 0 Successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("the name of progrme is %s\n", argv[0]);
+	return (0);
+}
