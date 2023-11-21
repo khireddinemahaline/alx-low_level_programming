a6ae95ef2b31e6026714a1a11e124e5a196d930e
khireddinemahaline <khirozr@gmail.com>
2023-11-21 01:29:11 -0800

    s

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry function
+ * @argc : number of arg
+ * @argv: atg
+ *
+ * Return 0 : successful
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
