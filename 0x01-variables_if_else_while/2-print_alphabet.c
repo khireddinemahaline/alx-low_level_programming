485e5084c128de88855fd6453d695348f2e89a28
khireddinemahaline <khirozr@gmail.com>
2024-02-21 04:51:17 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entery function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch="a";
+	while (ch <= "z")
+		putchar(ch);
+		ch ++;
+	putchar("\n");
+	return (0);
+}
