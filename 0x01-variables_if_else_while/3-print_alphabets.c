59c7ecbade74e3e6f4db00280873cfceca0b82be
khireddinemahaline <khirozr@gmail.com>
2024-02-21 05:12:36 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery function
+ *
+ * Return: Alays 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
