cf80169df058a4fb8aa94f045043e0bdbb63baed
khireddinemahaline <khirozr@gmail.com>
2024-02-22 09:19:53 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48, j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (j != 57 && i != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
