d35b1a5aebd52f5bb0e4160d14fbc9f54974a112
khireddinemahaline <khirozr@gmail.com>
2024-02-22 08:52:04 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	for (ch = 'A'; ch <= 'F'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
