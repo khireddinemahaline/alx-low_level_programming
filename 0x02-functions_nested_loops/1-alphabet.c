a945fb1ffdb3d689de5bf346ec49e288b6bb3476
khireddinemahaline <khirozr@gmail.com>
2024-02-23 00:13:01 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - function print alpha in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define HEADER
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
