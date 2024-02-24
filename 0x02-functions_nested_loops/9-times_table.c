9e184f6f5bc585a583631aff7763980eb3f174e9
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-24 14:48:52 +0100

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,11 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 57; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i*j);
+			_putchar((i + 48) * (j + 48));
 			_putchar(',');
 		}
 		_putchar('\n');
