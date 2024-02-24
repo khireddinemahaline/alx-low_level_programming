c5236d1a6dc5fa65e35e43c70a34ddac247cba68
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-24 15:49:09 +0100

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,24 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
-	{
-		if (n < 98)
+	if (n < 98)
+		while (n <= 98)
 		{
-			printf("%i, ", n);
+			if (n < 98)
+			{
+				printf("%i, ", n);
+			}
+			else
+				printf("%i\n", n);
+			n++;
 		}
-		else
-			printf("%i\n", n);
-		n++;
-	}
-	while (n > 98)
-	{
-		if (n > 98)
-			printf("%i ", n);
-		else
-			printf("%i\n", n);
-		i--;
-	}
+	else
+		while (n > 98)
+		{
+			if (n > 98)
+				printf("%i ", n);
+			else
+				printf("%i\n", n);
+			i--;
+		}
 }
