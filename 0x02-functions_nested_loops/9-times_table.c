b2f1474d21689a1321bd0b2b281189dcc56d1306
khireddinemahaline <khirozr@gmail.com>
2024-02-24 05:45:43 -0800

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - Function time 9 table
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int j, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 0; j <= 57; j++)
+		{
+			_putchar(i*j);
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
