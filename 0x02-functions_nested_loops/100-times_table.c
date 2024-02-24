2060cc8cc200de47e6d0792eb0b483aafc038590
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-24 17:36:16 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,6 +35,13 @@ void print_times_table(int n)
 					_putchar(k / 10 + '0');
 					_putchar(k % 10 + '0');
 				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k / 100 + '0');
+					_putchar(k % 100 + '0');
+				}
 			}
 			_putchar('\n');
 			i++;
