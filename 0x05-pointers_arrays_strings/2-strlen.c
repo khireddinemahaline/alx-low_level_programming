e467807e12076702b937303bc5b4890a68422911
khireddinemahaline <khirozr@gmail.com>
2024-03-04 01:36:44 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - function return the length of a string
+ * @s: pinter
+ * Return: the length of string
+ *
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i)
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _puts - function return string in stdout
+ * @str: string value
+ *
+ */
+void _puts(char *str)
+{
+	fprinf(stdout , str);
+}
