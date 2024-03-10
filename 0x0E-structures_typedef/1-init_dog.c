898e1987551a97b7175399a1194b25496dcc0eef
khireddinemahaline <khirozr@gmail.com>
2024-03-10 01:40:34 -0800

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
- * init_dog - entry func
- * @d : strct
- * @name  : naem of the dog
- * @age : age of the dog
- * @owner : owner of the dog
- * Return : no return
+ * init_dog - dog initial variable
+ * @d: a pinter point to struct dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
