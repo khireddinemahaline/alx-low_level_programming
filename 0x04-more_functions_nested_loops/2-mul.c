09d88428c64d55969b4192debe7550ded957350c
khireddinemahaline <khirozr@gmail.com>
2024-03-14 01:01:01 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiple two number
+ * @a: first int
+ * @b: seconde int
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int resulte;
+	resulte = a * b;
+	return resulte;
+}
