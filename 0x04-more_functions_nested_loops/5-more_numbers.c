0843fc6c4b1ce0262ff865c1b5320081dbe9155c
khireddinemahaline <khirozr@gmail.com>
2024-03-14 02:50:43 -0700

    6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,9 +4,16 @@
  */
 void more_numbers(void)
 {
-	char n;
+	int i, j;
 
-	for (n = '0'; n <= '14'; n++)
-		_putchar(n);
-	_putchar('\n');
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
 }
