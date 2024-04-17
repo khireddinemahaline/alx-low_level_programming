7698bc8d404f9042a9acf6418d49032da43179a2
khireddinemahaline <khirozr@gmail.com>
2024-04-17 18:30:49 +0100

    strin

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
     while (str[j] != '\0')
         j++;
 
-    for (i = 0; i <= j; i = i + 2)
+    for (i = 0; i <= j - 1; i = i + 2)
     {
         _putchar(str[i]);
     }
