28a1e4480d138746d88b879919f9fdd0658a1b63
khireddinemahaline <khirozr@gmail.com>
2024-06-11 11:09:45 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,10 +17,11 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
 	return (ptr);
-	free(ptr);
 }
