6e644e83e16ea32c2f317cf8e378f2a2ca43ec8a
khireddinemahaline <khirozr@gmail.com>
2024-06-11 14:23:28 -0400

    malooc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+char *create_array(unsigned int size, char c)
+{
+    unsigned int i;
+    char *ptr;
+
+    ptr = malloc(size * sizeof(c));
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < size; i++)
+    {
+        ptr[i] = c;
+    }
+    return (ptr);
+}
