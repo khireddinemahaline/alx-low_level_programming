4fb5b49d20f6b5df25a5927915945cef9da1bde4
khireddinemahaline <khirozr@gmail.com>
2024-06-15 10:58:46 -0700

    vardic func

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,12 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		int var = va_arg(args, int);
-
-		if (i < (n - 1))
-			printf("%d%s", var, separator);
-		else
-			printf("%d", var);
+		printf("%d", va_arg(args, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
 	putchar('\n');
 	va_end(args);
