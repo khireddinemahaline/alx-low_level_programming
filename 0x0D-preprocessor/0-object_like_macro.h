fb31d4bff0502a7dd7a57eeb3d938f90fb0eb698
khireddinemahaline <khirozr@gmail.com>
2024-06-17 15:06:53 -0400

    single linkedlist

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,1 @@
-#ifndef _OBJECT_MACRO
-#define _OBJECT_MACRO
-
-#define SIZE 1024
-
-#endif
+ 
--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,22 @@
 #include "lists.h"
+
+
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
-	if (h == NULL)
-		printf("[0] (nil)\n");
-	struct node *ptr = NULL;
-	ptr = h;
-	while (ptr != NULL)
+
+	while (h)
 	{
-		printf("[%d] %s\n", ptr->len, ptr->str);
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 		nodes++;
-		ptr = ptr->next;
+		
 	}
-	return (nodes);
+
+
+	return(nodes);
+
+}
