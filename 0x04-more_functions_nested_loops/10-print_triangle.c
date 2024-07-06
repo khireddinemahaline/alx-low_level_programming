db2dab8e1d1f2c966cd1035dea9d20165fdfb9a3
khireddinemahaline <khirozr@gmail.com>
2024-07-06 14:46:46 -0700

    add fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,19 +11,13 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (i = 1; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				if (j < i)
-				{
-					_putchar(' ');
-				}
-				else
-					_putchar('#');
-			}
+			for (j = i; j < size; j++)
+				_putchar(' ');
+			for (j = 1; j <= i; j++)
+				_putchar('#');
 			_putchar('\n');
-
 		}
 	}
 }
