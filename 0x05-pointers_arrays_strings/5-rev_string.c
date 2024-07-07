73a2bf83247d0cb7c89df15f6a4e93271dcceb9a
khireddinemahaline <khirozr@gmail.com>
2024-07-07 14:38:24 -0700

    fix reverse code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,17 @@
 
 void rev_string(char *s)
 {
-    char temp;
-    int len = 0, length, j;
+	char temp;
+	int len = 0, length, j;
 
-    while (s[len] != '\0')
-        len++;
-    length = len - 1;
-    for (j = 0; j <= length; j++)
-    {
-        temp = s[j];
-        s[j] = s[length];
-        s[length] = temp;
-	length--;
-    }
+	while (s[len] != '\0')
+		len++;
+	length = len - 1;
+	for (j = 0; j <= length; j++)
+	{
+		temp = s[j];
+		s[j] = s[length];
+		s[length] = temp;
+		length--;
+	}
 }
-
