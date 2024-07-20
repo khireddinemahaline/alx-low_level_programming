8c3e029f96b619c73517f078a10c63f573d2eae3
khireddinemahaline <khirozr@gmail.com>
2024-07-20 03:18:58 -0400

    binary

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,8 +3,6 @@
 /**
  * print_times_table - func
  * @n: value
- *
- *
  */
 void print_times_table(int n)
 {
@@ -31,23 +29,6 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(k / 10 + '0');
-					_putchar(k % 10 + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-			i++;
-		}
-	}
 	else
 		_putchar(';');
 }
