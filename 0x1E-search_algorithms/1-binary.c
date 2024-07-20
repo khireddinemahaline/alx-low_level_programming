5297b8912a719268a902ec0364650354ae1ee911
khireddinemahaline <khirozr@gmail.com>
2024-07-19 23:27:32 -0700

    binary algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,13 +9,12 @@
 
 void print_array(int *array, int low, int high)
 {
-    int i;
+	int i;
 
-    printf("Searching in array");
-
-    for (i = low; i < high; i++)
-        printf("%s %d", (i == low) ? ":" : ",", array[i]);
-    printf("\n");
+	printf("Searching in array");
+	for (i = low; i < high; i++)
+		printf("%s %d", (i == low) ? ":" : ",", array[i]);
+	printf("\n");
 }
 
 /**
@@ -23,30 +22,26 @@ void print_array(int *array, int low, int high)
 * @array: the array to serch in
 * @size: size of array
 * @value: value search for
+* Return: -1 if not found
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i = 0, low, high, midle;
-    low = i;
-    high = size - 1;
+	size_t i = 0, low, high, midle;
 
-    if (array == NULL || size == 0)
+	low = i;
+	high = size - 1;
+	if (array == NULL || size == 0)
 		return (-1);
-    printf("\n");
-    while (low <= high)
-    {
-        print_array(array, low, high + 1);
-        midle = (low + high) / 2;
-        if (value < array[midle])
-        {
-            high = midle - 1;
-        }
-        else if (value > array[midle])
-        {
-            low = midle + 1;
-        }
-        else if (value == array[midle])
-            return midle;
-    }
-    return (-1);
+	while (low <= high)
+	{
+		print_array(array, low, high + 1);
+		midle = (low + high) / 2;
+		if (value < array[midle])
+			high = midle - 1;
+		else if (value > array[midle])
+			low = midle + 1;
+		else if (value == array[midle])
+			return (midle);
+	}
+	return (-1);
 }
